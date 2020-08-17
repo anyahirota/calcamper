dfdf59192abdf72d4da2aa09d139b22c50d1a39e
anyahirota <anyahirota@gmail.com>
2020-08-17 13:39:37 -0700

    Create and test sessions api util functions (ajax requests)

--- frontend/calcamper.jsx
+++ frontend/calcamper.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//FOR TESTING
+import {login, signup, logout} from './util/session_api_util'
+
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
+
+    //FOR TESTING
+    window.login = login;
+    window.signup = signup;
+    window.logout = logout;
+    //FOR TESTING
+
+
+
     ReactDOM.render(<h1>Welcome to CalCamper</h1>, root);
 });

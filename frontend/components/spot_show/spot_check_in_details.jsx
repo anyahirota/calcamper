f90ab6947ce4e79dbb19835c458c890064a8978f
anyahirota <anyahirota@gmail.com>
2020-08-21 10:57:59 -0700

    Add spot listing checkin details

--- frontend/components/spot_show/spot_check_in_details.jsx
+++ frontend/components/spot_show/spot_check_in_details.jsx
@@ -3,8 +3,19 @@ import React from 'react'
 const SpotCheckInDetails = ({spot}) => {
     return (
         <div className="spot-check-in">
-            
-
+            <div className="spot-check-in-inner">
+                <p>Details</p>
+                <ul className="spot-check-in-col">
+                    <li><p>Check in: </p>{spot.check_in_details}</li>
+                    <li><p>Check out: </p>{spot.check_out_details}</li>
+                    <li><p>Cancellation policy: </p>{spot.cancellation_policy}</li>
+                </ul>
+                <ul className="spot-check-in-col">
+                    <li><p>On arrival: </p>{spot.on_arrival}</li>
+                    <li><p>Minimum nights: </p>{spot.minimum_nights}</li>
+                    <li><p>Accepts bookings: </p>{spot.booking_acceptance}</li>
+                </ul>
+            </div>
         </div>
     )
 }

46f3626a9ef5cd56a06d75eeb0922000ff59f8d7
anyahirota <anyahirota@gmail.com>
2020-08-28 11:14:40 -0700

    Add slash between react and redux in footer

--- frontend/components/footer/footer_content.jsx
+++ frontend/components/footer/footer_content.jsx
@@ -24,7 +24,7 @@ const FooterContent = () => {
                             Ruby on Rails
                         </li>
                         <li>
-                            ReactRedux
+                            React/Redux
                         </li>
                         <li>
                             HTML

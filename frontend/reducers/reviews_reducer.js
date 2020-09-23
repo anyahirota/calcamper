818552b87239fccb8b0ff3cd0b799d8a27f7ac3a
anyahirota <anyahirota@gmail.com>
2020-09-23 13:50:58 -0700

    Add reviews function to receive_spot action

--- frontend/reducers/reviews_reducer.js
+++ frontend/reducers/reviews_reducer.js
@@ -1,10 +1,18 @@
 import {RECEIVE_REVIEWS, REMOVE_REVIEW} from "../actions/review_actions"; 
+import { RECEIVE_SPOT } from '../actions/spot_actions'; 
 
 const reviewsReducer = (oldState = {}, action) => {
     Object.freeze(oldState); 
     switch(action.type) {
         case RECEIVE_REVIEWS:
             return action.reviews; 
+        
+        case RECEIVE_SPOT: 
+            if (action.payload.reviews) {
+                return action.payload.reviews; 
+            } else {
+                return {}; 
+            }; 
         
         case REMOVE_REVIEW: 
             let newState = {...oldState}; 

6fc29db35b9a1698048d6ea045e8c5ca5bb1aaf9
anyahirota <anyahirota@gmail.com>
2020-09-23 20:37:36 -0700

    Add spot review index styles

--- frontend/components/reviews/spot_review_index_item.jsx
+++ frontend/components/reviews/spot_review_index_item.jsx
@@ -1,20 +1,25 @@
 import React from "react"; 
 import { faUserCircle } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import moment from "moment";
 
 const SpotReviewIndexItem = ({review}) => {
+    const date = moment(review.created_at).format("MMMM Do[,] YYYY");
     const lastInitial = review.author_lname[0];
     return (
       <li className="spot-review-index-item">
         <div className="spot-review-author">
-            <p><FontAwesomeIcon icon={faUserCircle} /></p>
-            <p>{review.author_fname} {lastInitial}.</p>
+          <p>
+            <FontAwesomeIcon icon={faUserCircle} />
+          </p>
+          <p>
+            {review.author_fname} {lastInitial}.
+          </p>
         </div>
         <div className="spot-review-title-body">
-            <h1>{review.title}</h1>
-            {/* review date */}
-            <p>{review.body}</p>
+          <h1>{review.title}</h1>
+          <p className="spot-review-date">{date}</p>
+          <p>{review.body}</p>
         </div>
       </li>
     );
--- frontend/components/reviews/spot_reviews.jsx
+++ frontend/components/reviews/spot_reviews.jsx
@@ -1,11 +1,12 @@
 import React from "react"; 
 import SpotReviewIndex from "./spot_review_index"; 
+import ReviewCreateFormContainer from "./review_create_form_container"; 
 
 const SpotReviews =({spot, reviews}) => {
     return (
         <div className="spot-reviews">
             <h1>{reviews.length} Written reviews</h1>
-            {/* review form */}
+            <ReviewCreateFormContainer spot={spot}/>
             <SpotReviewIndex spot={spot} reviews={reviews}/>
         </div>
     )
